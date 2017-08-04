6e14be539b9bc1b9922e0201a878f52a918c3d41
kalipsco <kalipsco@gmail.com>
2017-08-03 22:52:02 -0400

    first push

---
+++ keys.jsx
@@ -0,0 +1,8 @@
+console.log('this is loaded');
+
+exports.twitterKeys = {
+  consumer_key: '< vg8FVvlICXknxbUvLTS2GsrwJ >',
+  consumer_secret: '< 30JjgwKN6fRGDgeZdsKkFFBPAAS7AcLnSHXEBhuntVPsgq6KR7 >',
+  access_token_key: '< 893198088038490114-clGAuN08Agdthf8FpuWC7Gw9OripfLp >',
+  access_token_secret: '< dPb8Yxul96nMxFhfirIZZxmux2OqoUCvZn0SuYJzHPK0b >',
+}
